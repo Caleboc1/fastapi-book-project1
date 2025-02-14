0fd0e37ff5de7d6767ad787c8c2330fce6acf512
caleb <cakpomughe@gmail.com>
2025-02-14 14:22:22 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from api.router import api_router
+from core.config import settings
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix=settings.API_PREFIX)
+
+@app.get("/healthcheck")
+async def health_check():
+    """Checks if server is active."""
+    return {"status": "active"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # This line is the issue!
+    uvicorn.run(app, host="0.0.0.0", port=port)
